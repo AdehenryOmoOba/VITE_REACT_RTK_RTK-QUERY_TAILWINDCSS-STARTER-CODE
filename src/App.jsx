b38831e9f9609982ab23bf35d48a632d4d0cc216
adehenry <adefemiadelaja@gmail.com>
2022-11-24 17:32:45 +0100

    starter code

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <main className="bg-blue-900/10 w-screen h-screen p-10 overflow-x-hidden">
+     <section className="grid place-items-center bg-blue-600 h-10 my-10">
+     <h1 className='text-center'>Welcome</h1>
+     </section>
+     <section className="grid place-items-center bg-blue-600 h-10 my-10">
+     <h1 className='text-center'>Welcome</h1>
+     </section>
+     <section className="grid place-items-center bg-blue-600 h-10 my-10">
+     <h1 className='text-center'>Welcome</h1>
+     </section>
+    </main>
+  )
+}
+
+export default App
